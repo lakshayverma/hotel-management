8a20305faaa325614233330e990c0495a03b0099
Lakshay Verma <verma_lakshay@live.in>
2019-10-14 15:26:37 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -8,11 +8,11 @@ defined('DB_PASS') ? null : define("DB_PASS", "");
 
 
 // Site Details
-defined('SITE_TITLE') ? null : define('SITE_TITLE', "Mann Hotel");
-defined('SITE_MOTO') ? null : define('SITE_MOTO', "A project for MCA");
-defined('DEVELOPER_NAME') ? null : define('DEVELOPER_NAME', "Mandeep Kaur");
-defined('DEVELOPER_INFO') ? null : define('DEVELOPER_INFO', "146438712");
-defined('DEVELOPER_MAIL') ? null : define('DEVELOPER_MAIL', "mandeepshabina@gmail.com");
-defined('DEVELOPER_TEL') ? null : define('DEVELOPER_TEL', "+919041447146");
+defined('SITE_TITLE') ? null : define('SITE_TITLE', "Hotel Management");
+defined('SITE_MOTO') ? null : define('SITE_MOTO', "A project for managing hotels");
+defined('DEVELOPER_NAME') ? null : define('DEVELOPER_NAME', "Lakshay Verma");
+defined('DEVELOPER_INFO') ? null : define('DEVELOPER_INFO', "265254001");
+defined('DEVELOPER_MAIL') ? null : define('DEVELOPER_MAIL', "verma_lakshay@live.in");
+defined('DEVELOPER_TEL') ? null : define('DEVELOPER_TEL', "+919779333346");
 defined('SITE_CURRENCY') ? null : define('SITE_CURRENCY', "₹");
 ?>
